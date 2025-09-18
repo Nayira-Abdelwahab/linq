a233d68202a1f69a404013d970599fe6b96dc37e
Nayira-Abdelwahab <nayiraabdelwahab@gmail.com>
2025-09-18 14:33:55 +0300

    Add files via upload

---
+++ session2-linq-final.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using CompanyEFCore.Models; 
+
+namespace CompanyEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new CompanyDbContext())
+            {
+                Console.WriteLine("List of Employees:");
+
+                var employees = context.Employees.ToList();
+
+                foreach (var emp in employees)
+                {
+                    Console.WriteLine($"ID: {emp.Id}, Name: {emp.Name}, DepartmentID: {emp.DepartmentId}");
+                }
+
+                var newEmployee = new Employee
+                {
+                    Name = "Nayira Abdelwahab",
+                    DepartmentId = 1,
+                    Salary = 8000
+                };
+
+                context.Employees.Add(newEmployee);
+                context.SaveChanges();
+                Console.WriteLine("New employee added successfully!");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
